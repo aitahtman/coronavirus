7d6f0e1e4b608966d35264bf3a64055c87adf74f
Salah Eddine <salah-eddine.ait-ahtman@inra.fr>
2020-01-30 09:55:41 +0100

    init app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # coronavirus
+
+this application aims to follow the evolution of Corona Virus all over the world.
+
+
+# DATA USED
+the dataset used is provided and daily updated by JHU CSSE 
+
+
---
+++ app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapComponent } from './map/map.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
