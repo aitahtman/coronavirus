1b85675a4bde2986bdfaaf59a9921d5e74a4bda0
Salah Eddine <aitahtman.salaheddine@gmail.com>
2020-02-01 19:16:30 +0100

    Auto-generated commit

--- app/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  mapbox:{
-    accessToken: 'pk.eyJ1IjoiYWl0YWh0bWFuIiwiYSI6ImNqNG50emUwaTF2dWsyd283M2VhY2FpaTcifQ.eOs9cdOfs_YlXpO1gu2uWA'
-  },
-  dataUrl:"https://api.jajjferris.com/coronavirus"
-};
