ab799ae9ca9163a77c6845b2b279350b2def7863
AIT AHTMAN Salah Eddine <aitahtman.salaheddine@gmail.com>
2020-03-14 00:46:15 +0100

    build 🏗️ : 0.2.0

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FlexLayoutModule,GridModule } from '@angular/flex-layout';
 import { MapComponent } from './components/map/map.component';
 import { InfoComponent } from './components/info/info.component';
 import { MatProgressBarModule, MatCardModule } from '@angular/material/';
-import { CountUpModule } from 'ngx-countup';
+import { CountUpModule } from 'countup.js-angular2';
 import { EvolutionGraphComponent } from './components/evolution-graph/evolution-graph.component';
 import { ChartsModule } from 'ng2-charts';
 
